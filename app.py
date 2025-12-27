ba5cbe453202cfbe6c7fb36313f69e0b110bbeb6
anjalireddyy001-oss <anjalireddyy001@gmail.com>
2025-12-27 23:06:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,7 @@ def index():
 
     return render_template("index.html", result=result, drug=drug_name)
 
-if __name__ == "__main__": 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
